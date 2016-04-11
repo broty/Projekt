2dac71de6f0d67c7b8d209b2a30041c7d580e9de
martin brotanek <martin.brotanek@tul.cz>
2016-04-11 15:12:40 +0200

    projekt

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Martin on 11. 4. 2016.
+ */
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
