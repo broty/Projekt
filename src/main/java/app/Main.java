e9ea7b0111d7f7aa005285ef7cd914a49667b80f
martin brotanek <martin.brotanek@tul.cz>
2016-05-02 15:47:34 +0200

    webapp

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,12 +2,33 @@ package app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by Martin on 11. 4. 2016.
  */
 
 @SpringBootApplication
-public class Main {
+public class Main extends HttpServlet {
+    private String message;
+
+    public void init() throws ServletException {
+        message = "Hello World";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
     public static void main(String[] args) throws Exception {
 
     }
